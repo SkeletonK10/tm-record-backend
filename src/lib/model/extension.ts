419b19988d3fc546756029b54b5bc45e8d12c613
skeletonk10 <poohbilly2@naver.com>
2021-12-29 07:20:44 +0900

    #6 - Create Extension scheme

--- src/lib/model/extension.ts
+++ src/lib/model/extension.ts
@@ -1,5 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * A Extension of game
+ * @typedef {object} Extension
+ * @property {number} id.required - Extension Identifier
+ * @property {string} name.required - Extension name
+ */
+
 @Entity()
 export default class Extension {
   @PrimaryGeneratedColumn()
