13731bfb150228f0f5b4ffe6f0c564371245e788
skeletonk10 <poohbilly2@naver.com>
2021-12-29 07:16:51 +0900

    fix #5 - create map scheme

--- src/lib/model/map.ts
+++ src/lib/model/map.ts
@@ -1,5 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * A Map
+ * @typedef {object} Map
+ * @property {number} id.required - Map Identifier
+ * @property {string} name.required - Map name
+ */
+
 @Entity()
 export default class Map {
   @PrimaryGeneratedColumn()
