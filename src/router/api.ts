15b999b49f438fdecf80dfd41df237f677f60451
skeletonk10 <poohbilly2@naver.com>
2022-01-03 05:14:33 +0900

    Modified main page for more info

--- src/router/api.ts
+++ src/router/api.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from 'express';
 
+const mainMsg: string = `Hello, this is the backend server for Terraforming Mars Recording system!<br>
+  See /api-docs for API specification.`;
+
 export const mainGet: RequestHandler = (req, res, next) => {
-  res
-    .status(200)
-    .json({ text: 'GO WORK FRONTEND WE DO NOT HAVE ANY INFORMATION FOR IT' });
+  res.status(200).send(mainMsg);
   return next();
 };
