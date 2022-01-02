79665e504a2e6329e34f69359306637784fad32d
skeletonk10 <poohbilly2@naver.com>
2022-01-02 23:29:33 +0900

    #14 - Rearrangement of API Schemes' location

--- src/lib/model/game.ts
+++ src/lib/model/game.ts
@@ -11,17 +11,6 @@ import Extension from './extension';
 import Map from './map';
 import Result from './result';
 
-/**
- * A Game
- * @typedef {object} Game
- * @property {number} id.required - Game Identifier
- * @property {boolean} valid.required - true if the game is successfully over
- * @property {number} season.required - Season participated (Pre-season: 0)
- * @property {Map} map.required - Map played
- * @property {array<Extension>} extensions - Extensions played
- * @property {array<Result>} results.required - Results engaged
- */
-
 @Entity()
 export default class Game {
   @PrimaryGeneratedColumn()
@@ -32,6 +21,9 @@ export default class Game {
 
   @Column()
   season!: number;
+
+  @Column()
+  round!: number;
 
   @OneToOne(() => Map)
   map!: Map;
