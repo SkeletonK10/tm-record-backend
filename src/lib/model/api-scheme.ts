908423ed144177d90bd8abbd1b45b8874030a73c
skeletonk10 <poohbilly2@naver.com>
2022-01-02 23:31:17 +0900

    #14 - User Scheme modification

--- src/lib/model/api-scheme.ts
+++ src/lib/model/api-scheme.ts
@@ -49,6 +49,5 @@
  * @property {number} id.required - User Identifier
  * @property {string} nickName - User nickname
  * @property {string} realName.required - User's real name
- * @property {number} rating.required - User Rating (Default: 1500)
- * @property {array<Result>} results - All results user played
+ * @property {number} rating.required - User Rating
  */
